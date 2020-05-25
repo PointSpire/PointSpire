166c9b1060c0a725b4f5335823b6bff0773bab69
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-24 20:41:15 -0700

    US66T74 Setup initial config needed for ESLint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        PointSpire
-      </header>
+      <header className="App-header">PointSpire</header>
     </div>
   );
 }
