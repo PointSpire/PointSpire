dbce887588aa270ab849d37219dc5c668572b1e5
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-25 14:46:37 -0700

    US63T78 Add subtasks logic to projects endpoint

--- node_server/src/main/routes/projects.ts
+++ node_server/src/main/routes/projects.ts
@@ -1,8 +1,23 @@
 import express, { Router } from 'express';
 import mongoose from 'mongoose';
-import { ProjectModel, createProjectModel } from '../models/project';
+import {
+  ProjectModel,
+  ProjectDoc,
+  createProjectModel,
+} from '../models/project';
+import { createTaskModel, TaskModel } from '../models/task';
 
 const router = express.Router();
+
+const errorDescriptions = {
+  mongoProjectFindErr: 'There was an error while finding the project.',
+  projectNotFound(id: string): string {
+    return `The project with ID: ${id} was not found.`;
+  },
+  taskNotDefined:
+    `The task was not defined either with a proper body or with` +
+    ` the "taskTitle" defined.`,
+};
 
 /**
  * Creates the express Router for the `/projects` endpoint.
@@ -12,6 +27,7 @@ const router = express.Router();
  */
 function createProjectsRouter(db: typeof mongoose): Router {
   const Project: ProjectModel = createProjectModel(db);
+  const Task: TaskModel = createTaskModel(db);
 
   router.get('/', (req, res) => {
     res.status(405);
@@ -21,18 +37,56 @@ function createProjectsRouter(db: typeof mongoose): Router {
     );
   });
 
-  router.get('/projects', (req, res, next) => {
-    Project.find().exec((err, tasks) => {
+  router.get('/:projectId', (req, res, next) => {
+    Project.find({ _id: req.params.projectId }).exec((err, projects) => {
       if (err) {
-        console.error(
-          'There was an error while querying tasks in the database'
-        );
+        console.error(errorDescriptions.mongoProjectFindErr);
         next(err);
       } else {
-        res.json(tasks);
+        res.json(projects);
       }
     });
   });
+
+  /**
+   * Creates a new subtask for the given project ID.
+   */
+  router.post('/:projectId/subtasks', (req, res, next) => {
+    // Create a promise to chain the logic
+    new Promise<ProjectDoc>((resolve, reject) => {
+      Project.find({ _id: req.params.projectId }).exec((err, projects) => {
+        if (err) {
+          const updatedErr = Object.assign({}, err, {
+            additionalMessage: errorDescriptions.mongoProjectFindErr,
+          });
+          reject(updatedErr);
+        } else if (projects.length === 0) {
+          const err = new Error(
+            errorDescriptions.projectNotFound(req.params.projectId)
+          );
+          reject(err);
+        } else {
+          resolve(projects[0]);
+        }
+      });
+    })
+      .then(projectDoc => {
+        if (req.body && req.body.taskTitle) {
+          const newTask = new Task({
+            title: req.body.taskName,
+          });
+          newTask.save();
+          projectDoc.subtasks.push(newTask._id);
+          projectDoc.save();
+        } else {
+          throw new Error(errorDescriptions.taskNotDefined);
+        }
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
   return router;
 }
 
