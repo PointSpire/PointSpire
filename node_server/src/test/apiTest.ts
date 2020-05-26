78cfd73917fa94411e8f91caf549ab815ded6454
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-26 14:23:56 -0700

    US62T70 Shrink down apiTest to what is needed

--- node_server/src/test/apiTest.ts
+++ node_server/src/test/apiTest.ts
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../../app';
+import Globals from './globals';
 
 // Configure chai
 chai.use(chaiHttp);
@@ -8,44 +8,33 @@ chai.use(chaiHttp);
 // Use the assert style
 const assert = chai.assert;
 
-/**
- * Waits for the server to be started before running tests.
- */
-before(done => {
-  app.on('started', () => {
-    done();
+describe('GET /api/tasks', () => {
+  it('should get all tasks from the MongoDB', done => {
+    chai
+      .request(Globals.app)
+      .get('/api/tasks')
+      .end((err, res) => {
+        assert.isNull(err);
+        assert.equal(res.status, 200);
+        assert.typeOf(res.body, 'array');
+        done();
+      });
   });
 });
-
-describe('/api endpoint', () => {
-  describe('GET /tasks', () => {
-    it('should get all tasks from the MongoDB', done => {
-      chai
-        .request(app)
-        .get('/api/tasks')
-        .end((err, res) => {
-          assert.isNull(err);
-          assert.equal(res.status, 200);
-          assert.typeOf(res.body, 'array');
-          done();
-        });
-    });
-  });
-  describe('GET /projects', () => {
-    it('should return a 405 and request an ID', done => {
-      chai
-        .request(app)
-        .get('/api/projects')
-        .end((err, res) => {
-          assert.isNull(err);
-          assert.equal(res.status, 405);
-          assert.equal(
-            res.text,
-            'Please specify a project ID by using /api/projects/24 where ' +
-              '"24" is the ID of the project.'
-          );
-          done();
-        });
-    });
+describe('GET /api/projects', () => {
+  it('should return a 405 and request an ID', done => {
+    chai
+      .request(Globals.app)
+      .get('/api/projects')
+      .end((err, res) => {
+        assert.isNull(err);
+        assert.equal(res.status, 405);
+        assert.equal(
+          res.text,
+          'Please specify a project ID by using /api/projects/24 where ' +
+            '"24" is the ID of the project.'
+        );
+        done();
+      });
   });
 });
