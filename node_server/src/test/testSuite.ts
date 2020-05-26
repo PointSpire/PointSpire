d39199e1337d24000cdf1e49ac331f0c92a3d74b
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-26 14:27:06 -0700

    US62T70 Update testSuite documentation

--- node_server/src/test/testSuite.ts
+++ node_server/src/test/testSuite.ts
@@ -1,13 +1,14 @@
 /**
  * This file is used to specify the order in which the tests run. The order of
- * imports below is the order in which they run. This is also used to pass
- * variables between the files and cleanup after the tests.
+ * require statements below is the order in which they run. This is also used
+ * to pass variables between the files and cleanup after the tests.
  *
  * We are looking to achieve the following logic:
  * 1. Connect to the database
  * 2. Test that a user can be created and pass that created user on to the
  * next tests.
  * 3. Run the rest of the tests with the connected app and testUser.
+ * 4. Delete the test user from the DB.
  */
 
 import app from '../../app';
