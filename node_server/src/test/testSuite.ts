ff39b622643e4cebe21b1bdf16ba4ca6a07ba650
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-26 14:23:18 -0700

    US62T70 Add a testSuite to contain our other tests

---
+++ node_server/src/test/testSuite.ts
@@ -0,0 +1,30 @@
+/**
+ * This file is used to specify the order in which the tests run. The order of
+ * imports below is the order in which they run. This is also used to pass
+ * variables between the files and cleanup after the tests.
+ *
+ * We are looking to achieve the following logic:
+ * 1. Connect to the database
+ * 2. Test that a user can be created and pass that created user on to the
+ * next tests.
+ * 3. Run the rest of the tests with the connected app and testUser.
+ */
+
+import app from '../../app';
+import Globals from './globals';
+
+/**
+ * Waits for the server to be started before running tests
+ */
+before(done => {
+  app.on('started', () => {
+    Globals.app = app;
+    done();
+  });
+});
+
+describe('PointSpire tests', () => {
+  require('./testStartup');
+  require('./apiTest');
+  require('./testCleanup');
+});
