b0d05d4c26fea5cfc86af49f6349418926a417a9
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-26 18:43:45 -0700

    US63T78 Add test for getting project with id

--- node_server/src/test/apiTest.ts
+++ node_server/src/test/apiTest.ts
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Globals from './globals';
+import { ProjectDoc } from '../main/models/project';
 
 // Configure chai
 chai.use(chaiHttp);
@@ -21,20 +22,51 @@ describe('GET /api/tasks', () => {
       });
   });
 });
-describe('GET /api/projects', () => {
-  it('should return a 405 and request an ID', done => {
-    chai
-      .request(Globals.app)
-      .get('/api/projects')
-      .end((err, res) => {
-        assert.isNull(err);
-        assert.equal(res.status, 405);
-        assert.equal(
-          res.text,
-          'Please specify a project ID by using /api/projects/24 where ' +
-            '"24" is the ID of the project.'
-        );
-        done();
-      });
+describe('api/projects', () => {
+  describe('GET', () => {
+    it('should return a 405 and request an ID', done => {
+      chai
+        .request(Globals.app)
+        .get('/api/projects')
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.equal(res.status, 405);
+          assert.equal(
+            res.text,
+            'Please specify a project ID by using /api/projects/24 where ' +
+              '"24" is the ID of the project.'
+          );
+          done();
+        });
+    });
+  });
+  describe('GET /id', () => {
+    it('should return the project specified by the given id if the id is valid', done => {
+      // Add a project to test
+      chai
+        .request(Globals.app)
+        .post(`/api/users/${Globals.testUser._id}/projects`)
+        .send({
+          projectTitle: 'testProject',
+        })
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.typeOf(res.body, 'object');
+          assert.typeOf(res.body._id, 'string');
+          assert.equal(res.body.title, 'testProject');
+          const testProject: ProjectDoc = res.body;
+
+          // Make sure the project is being returned
+          chai
+            .request(Globals.app)
+            .get(`/api/projects/${testProject._id}`)
+            .end((err, res) => {
+              assert.isNull(err);
+              assert.equal(res.status, 200);
+              assert.deepEqual(res.body, testProject);
+              done();
+            });
+        });
+    });
   });
 });
