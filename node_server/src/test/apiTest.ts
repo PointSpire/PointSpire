cd3830502b961485d00937123466ff0c7e3a2ef3
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-05-29 11:40:55 -0700

    US63T78 Add delete test and subtasks test

--- node_server/src/test/apiTest.ts
+++ node_server/src/test/apiTest.ts
@@ -2,6 +2,8 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Globals from './Globals';
 import { ProjectDoc } from '../main/models/project';
+import { UserDoc } from '../main/models/user';
+import { TaskDoc } from '../main/models/task';
 
 // Configure chai
 chai.use(chaiHttp);
@@ -99,16 +101,52 @@ describe('api/projects', () => {
     it(
       'should delete the project with the id from the projects collection and' +
         ' from the user which has that project',
-      done => {
-        assert.fail('Not implemented');
-        done();
+      async () => {
+        const testProject = await generateTestProject();
+
+        // Send the delete request
+        const deleteRes = await chai
+          .request(Globals.app)
+          .delete(`/api/projects/${testProject._id}`);
+        assert.equal(deleteRes.status, 200);
+        assert.typeOf(deleteRes.body, 'object');
+        assert.equal(deleteRes.body._id, testProject._id);
+
+        // Check that the project doesn't come back
+        const projectRes = await chai
+          .request(Globals.app)
+          .get(`/api/projects/${testProject._id}`);
+        assert.equal(projectRes.status, 400);
+
+        // Make sure the project is deleted out of the testUser projects
+        const userRes = await chai
+          .request(Globals.app)
+          .get(`/api/users/${Globals.testUser._id}`);
+        const returnedUserDoc: UserDoc = userRes.body;
+        assert.equal(returnedUserDoc.projects.includes(testProject._id), false);
       }
     );
   });
   describe('POST /id/subtasks', () => {
-    it('should add a subtask if valid content is sent', done => {
-      assert.fail('Not implemented');
-      done();
+    it('should add a subtask if valid content is sent', async () => {
+      const testProject = await generateTestProject();
+      const res = await chai
+        .request(Globals.app)
+        .post(`/api/projects/${testProject._id}/subtasks`)
+        .send({
+          title: 'Some new task',
+          note: 'Some task note',
+        });
+      assert.equal(res.status, 201);
+      assert.typeOf(res.body, 'object');
+      assert.equal(res.body.title, 'Some new task');
+      assert.equal(res.body.note, 'Some task note');
+      const newTask: TaskDoc = res.body;
+      const projectRes = await chai
+        .request(Globals.app)
+        .get(`/api/projects/${testProject._id}`);
+      const returnedProject: ProjectDoc = projectRes.body;
+      assert.equal(returnedProject.subtasks.includes(newTask._id), true);
     });
     it('should not add a subtask if invalid content is sent', done => {
       assert.fail('Not implemented');
