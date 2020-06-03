ce4e78ee8ea89fc2a76fa1d6297ac18d054fda51
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-03 06:28:40 -0700

    US60T67 Add component definitions to swagger.tss

--- node_server/src/main/swagger.ts
+++ node_server/src/main/swagger.ts
@@ -13,6 +13,174 @@ const definition = {
     title: 'PointSpire API',
     description: 'API for querying the MongoDB for PointSpire',
   },
+  components: {
+    parameters: {
+      userIdParam: {
+        name: 'userId',
+        in: 'path',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+      projectIdParam: {
+        name: 'projectId',
+        in: 'path',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    },
+    schemas: {
+      userObjectBasis: {
+        type: 'object',
+        properties: {
+          userName: {
+            type: 'string',
+          },
+          firstName: {
+            type: 'string',
+          },
+          lastName: {
+            type: 'string',
+          },
+          githubId: {
+            type: 'string',
+          },
+          dateCreated: {
+            description:
+              'A Date string representing the date this user was created. This can be used as a valid Date object in JavaScript.',
+            type: 'string',
+          },
+          _id: {
+            type: 'string',
+          },
+          __v: {
+            type: 'integer',
+          },
+        },
+      },
+      userObjectProjectIds: {
+        type: 'object',
+        properties: {
+          projects: {
+            type: 'array',
+            description:
+              'An array of ObjectIds associated with the projects under this user.',
+            items: {
+              type: 'string',
+            },
+          },
+        },
+      },
+      userObjectProjects: {
+        type: 'object',
+        properties: {
+          projects: {
+            type: 'array',
+            items: {
+              $ref: '#/components/schemas/projectObjectWithTasks',
+            },
+          },
+        },
+      },
+      userObjectWithIds: {
+        allOf: [
+          {
+            $ref: '#/components/schemas/userObjectBasis',
+          },
+          {
+            $ref: '#/components/schemas/userObjectProjectIds',
+          },
+        ],
+      },
+      userObjectWithProjects: {
+        allOf: [
+          {
+            $ref: '#/components/schemas/userObjectBasis',
+          },
+          {
+            $ref: '#/components/schemas/userObjectProjects',
+          },
+        ],
+      },
+      projectObjectBasis: {
+        type: 'object',
+        properties: {
+          title: {
+            type: 'string',
+          },
+          note: {
+            type: 'string',
+          },
+          date: {
+            type: 'object',
+            description:
+              'A Date object representing the date this project was created.',
+          },
+        },
+      },
+      projectObjectTaskIds: {
+        type: 'object',
+        properties: {
+          subtasks: {
+            type: 'array',
+            items: {
+              type: 'string',
+            },
+          },
+        },
+      },
+      projectObjectTasks: {
+        type: 'object',
+        properties: {
+          subtasks: {
+            type: 'array',
+            items: {
+              $ref: '#/components/schemas/taskObject',
+            },
+          },
+        },
+      },
+      projectObjectWithIds: {
+        allOf: [
+          {
+            $ref: '#/components/schemas/projectObjectBasis',
+          },
+          {
+            $ref: '#/components/schemas/projectObjectTaskIds',
+          },
+        ],
+      },
+      projectObjectWithTasks: {
+        allOf: [
+          {
+            $ref: '#/components/schemas/projectObjectBasis',
+          },
+          {
+            $ref: '#/components/schemas/projectObjectTasks',
+          },
+        ],
+      },
+      taskObject: {
+        type: 'object',
+        properties: {
+          title: {
+            type: 'string',
+          },
+          note: {
+            type: 'string',
+          },
+          date: {
+            type: 'object',
+            description:
+              'A Date object representing the date this task was created.',
+          },
+        },
+      },
+    },
+  },
 };
 
 const options = {
