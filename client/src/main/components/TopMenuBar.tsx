f27e7d28e04febe77bf04de0539c60222dcc73a6
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-10 16:31:25 -0700

    US96T104 Add settings menu on the left hand side

--- client/src/main/components/TopMenuBar.tsx
+++ client/src/main/components/TopMenuBar.tsx
@@ -5,9 +5,16 @@ import {
   IconButton,
   Typography,
   Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import SettingsIcon from '@material-ui/icons/Settings';
+import HelpIcon from '@material-ui/icons/Help';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,11 +27,55 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
   })
 );
 
 function TopMenuBar(): JSX.Element {
   const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    drawerOpen: false,
+  });
+
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ): void => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, drawerOpen: open });
+  };
+
+  const list = (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Settings', 'Help'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index === 0 ? <SettingsIcon /> : <HelpIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
@@ -35,6 +86,7 @@ function TopMenuBar(): JSX.Element {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
@@ -44,6 +96,13 @@ function TopMenuBar(): JSX.Element {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="left"
+        open={state.drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        {list}
+      </Drawer>
     </div>
   );
 }
