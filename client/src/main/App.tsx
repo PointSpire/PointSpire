d3f6427ddad73abcab1aec2c6cba83f54035b972
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-10 15:29:07 -0700

    US96T104 Add initial top menu bar

--- client/src/main/App.tsx
+++ client/src/main/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import TopMenuBar from './components/TopMenuBar';
 
 function App(): JSX.Element {
   return (
     <div className="App">
+      <TopMenuBar />
       <header className="App-header">PointSpire</header>
     </div>
   );
---
+++ client/src/main/components/TopMenuBar.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+function TopMenuBar(): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default TopMenuBar;
