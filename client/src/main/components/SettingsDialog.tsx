84c1efcf2085c8fede5e4862ad2737c3308993c5
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-12 16:55:05 -0700

    US96T104 Add new section to settings

--- client/src/main/components/SettingsDialog.tsx
+++ client/src/main/components/SettingsDialog.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { Typography, Button, Dialog } from '@material-ui/core';
 
 type SettingsDialogProps = {
   open: boolean;
@@ -50,7 +49,9 @@ export default function SettingsDialog(
           ref={descriptionElementRef}
           tabIndex={-1}
         >
-          Some settings here
+          <Typography variant="h4" component="h2">
+            Something
+          </Typography>
         </DialogContentText>
       </DialogContent>
       <DialogActions>
