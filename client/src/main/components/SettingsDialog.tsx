5c4bd802a79bd245cbc8994c3cf8cd766dbbd7a8
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-12 19:42:33 -0700

    US96T104 Add a setting to the settings dialog

--- client/src/main/components/SettingsDialog.tsx
+++ client/src/main/components/SettingsDialog.tsx
@@ -1,67 +1,125 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { Typography, Button, Dialog } from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Dialog,
+  FormGroup,
+  Checkbox,
+  FormControlLabel,
+} from '@material-ui/core';
 
 type SettingsDialogProps = {
   open: boolean;
   setOpen: (open: boolean) => void;
 };
 
-export default function SettingsDialog(
-  props: SettingsDialogProps
-): JSX.Element {
-  const { setOpen, open } = props;
+type SettingsDialogState = {
+  settingYellowGreen: boolean;
+};
 
-  function handleClose(): void {
-    setOpen(false);
+export default class SettingsDialog extends React.Component<
+  SettingsDialogProps,
+  SettingsDialogState
+> {
+  descriptionElementRef = createRef<HTMLElement>();
+
+  constructor(props: SettingsDialogProps) {
+    super(props);
+    this.state = {
+      settingYellowGreen: true,
+    };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
   }
 
-  function handleSave(): void {
-    setOpen(false);
-    // Save their settings
-  }
-
-  const descriptionElementRef = React.useRef<HTMLElement>(null);
-  React.useEffect(() => {
+  componentDidMount(): void {
+    const { open } = this.props;
     if (open) {
-      const { current: descriptionElement } = descriptionElementRef;
+      const { current: descriptionElement } = this.descriptionElementRef;
       if (descriptionElement !== null) {
         descriptionElement.focus();
       }
     }
-  }, [open]);
+  }
 
-  return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      scroll="paper"
-      aria-labelledby="settings-dialog-title"
-      aria-describedby="settings-dialog-description"
-    >
-      <DialogTitle id="settings-dialog-title">Settings</DialogTitle>
-      <DialogContent dividers>
-        <DialogContentText
-          id="settings-dialog-description"
-          ref={descriptionElementRef}
-          tabIndex={-1}
-        >
-          <Typography variant="h4" component="h2">
-            Something
-          </Typography>
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleSave} color="primary">
-          Save
-        </Button>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+  handleClose(): void {
+    const { setOpen } = this.props;
+    setOpen(false);
+  }
+
+  handleSave(): void {
+    const { setOpen } = this.props;
+    setOpen(false);
+    // Save their settings
+  }
+
+  handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { name, checked } = event.target;
+    this.setState(state => {
+      return {
+        ...state,
+        [name]: checked,
+      };
+    });
+  }
+
+  render(): JSX.Element {
+    const { open } = this.props;
+    const {
+      handleClose,
+      handleSave,
+      handleCheckboxChange,
+      descriptionElementRef,
+    } = this;
+    const { settingYellowGreen } = this.state;
+
+    return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll="paper"
+        aria-labelledby="settings-dialog-title"
+        aria-describedby="settings-dialog-description"
+      >
+        <DialogTitle id="settings-dialog-title">Settings</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText
+            id="settings-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <Typography variant="h5" component="h2">
+              Display
+            </Typography>
+            <FormGroup row={false}>
+              <FormControlLabel
+                label="Make tasks yellow and green"
+                control={
+                  <Checkbox
+                    checked={settingYellowGreen}
+                    color="primary"
+                    name="settingYellowGreen"
+                    onChange={handleCheckboxChange}
+                  />
+                }
+              />
+            </FormGroup>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
