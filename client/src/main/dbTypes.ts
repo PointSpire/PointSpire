018da53b0e11a905454c0937b71c4199e6129a93
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-14 18:46:26 -0700

    US96T103 Add server types for User, Project, Task

--- client/src/main/dbTypes.ts
+++ client/src/main/dbTypes.ts
@@ -3,16 +3,51 @@ type Document = {
   __v: number;
 };
 
+/* User Types */
+
+export type UserSettings = {
+  yellowGreenTasks: boolean;
+
+  /**
+   * This is defined so that extra settings can be added onto the settings
+   * object
+   */
+  [settingName: string]: boolean;
+};
+
 export interface User extends Document {
-  projects: Array<Project>;
+  projects: Array<string>;
+  firstName: string;
+  lastName: string;
+  githubId: string;
   dateCreated: Date;
   userName: string;
+  settings: UserSettings;
 }
+
+export type AllUserData = {
+  user: User;
+  projects: ProjectObjects;
+  tasks: TaskObjects;
+};
+
+/* Project Types */
 
 export type Project = Task;
 
+export type ProjectObjects = {
+  [id: string]: Project;
+};
+
+/* Task Types */
+
 export interface Task extends Document {
-  subtasks: Array<Task>;
+  subtasks: Array<string>;
   dateCreated: Date;
+  note: string;
   title: string;
 }
+
+export type TaskObjects = {
+  [id: string]: Task;
+};
