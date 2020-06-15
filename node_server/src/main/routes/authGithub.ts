b67e9cf6107716bdb2f01b3d0e521a042de347d9
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-15 16:21:07 -0700

    US65T72 Convert passport auth to manual auth

--- node_server/src/main/routes/authGithub.ts
+++ node_server/src/main/routes/authGithub.ts
@@ -1,35 +1,71 @@
 import express, { Router } from 'express';
-import passport from 'passport';
+import fetch from 'node-fetch';
+import {
+  createUserObjectGithub,
+  saveOrFindNewGithubUser,
+} from '../../lib/userLib';
+import mongoose from 'mongoose';
 
 const router = express.Router();
-
-// const errorDescriptions = {
-//   mongoUserFindErr: 'There was an error while finding the user.',
-//   userNotFound(id: string): string {
-//     return `The user with ID: ${id} was not found.`;
-//   },
-//   projectNotDefined:
-//     `The project was not defined either with a proper body or with` +
-//     ` the "projectTitle" defined.`,
-//   userUpdateNotDefined:
-//     `No content was provided in the body to update ` + `the user with.`,
-//   newUserDetailsNotDefined:
-//     `Details for the new user were not specified in ` +
-//     `the body of the message. Please define at least the userName in the body.`,
-// };
 
 /**
  * Creates the express Router for the `/auth/github` endpoint.
  *
+ * @param {typeof mongoose} db the connected mongoDB instance
  * @returns {Router} the Router for the `/auth/github` endpoint
  */
-function authGithubRouter(): Router {
-  router.get('/', passport.authenticate('github'));
+function authGithubRouter(db: typeof mongoose): Router {
+  router.options('/', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
-  router.get(
-    '/callback',
-    passport.authenticate('github', { failureRedirect: '/login' })
-  );
+  router.post('/', async (req, res) => {
+    try {
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      if (req.body && req.body.code) {
+        const githubReqBody = {
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          client_id: process.env.GITHUB_CLIENT_ID,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code: req.body.code,
+        };
+        const githubRes = await fetch(
+          `https://github.com/login/oauth/access_token`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+            },
+            body: JSON.stringify(githubReqBody),
+          }
+        );
+        const githubToken = (await githubRes.json()).access_token;
+        const profileRes = await fetch(`https://api.github.com/user`, {
+          method: 'GET',
+          headers: {
+            Authorization: `token ${githubToken}`,
+          },
+        });
+        const githubProfile = await profileRes.json();
+        const newUser = createUserObjectGithub(db, githubProfile);
+        const userDoc = await saveOrFindNewGithubUser(db, newUser);
+        if (req.session) {
+          req.session.userId = userDoc._id;
+        }
+        console.log('Retrieved user: ', JSON.stringify(userDoc, null, 2));
+        res.status(200);
+        res.json(userDoc);
+      } else {
+        res.json({ error: 'Must include req.body.code' });
+      }
+    } catch (err) {
+      res.status(400);
+      res.send(err);
+    }
+  });
 
   return router;
 }
