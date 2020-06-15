1fca132dfa1b6c17fceaea85433db67b9dd3aa77
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-15 16:20:00 -0700

    US65T72 Adapt userLib to promises

--- node_server/src/lib/userLib.ts
+++ node_server/src/lib/userLib.ts
@@ -1,15 +1,20 @@
 import { UserModel, createUserModel, UserDoc } from '../main/models/user';
 import mongoose from 'mongoose';
 
+type GithubProfile = {
+  id: string;
+  username: string;
+};
+
 /**
  *
  * @param {mongoose} db The connected MnogoDB object
- * @param {any} profile // TODO create Github profile type
+ * @param {GithubProfile} profile // TODO create Github profile type
  * @returns {UserDoc} newUser
  */
 export function createUserObjectGithub(
   db: typeof mongoose,
-  profile: any // TODO create Github profile type
+  profile: GithubProfile
 ): UserDoc {
   const User: UserModel = createUserModel(db);
   let newUser = new User({ userName: profile.username });
@@ -24,48 +29,19 @@ export function createUserObjectGithub(
 
 /**
  *
- * @param {mongoose} db The connected MnogoDB object
- * @param {stirng} githubId GithubId of user account
- * @param {Function} userFound Callback if user is found in database
- * @param {Function} userNotFound Callback if use is not found in database
+ * @param {typeof mongoose} db the connected MongoDB instance
+ * @param {UserDoc} newUser the tentatively created newUser
  */
-function checkUserGithubId(
+export async function saveOrFindNewGithubUser(
   db: typeof mongoose,
-  githubId: string,
-  userFound: Function,
-  userNotFound: Function
-): void {
+  newUser: UserDoc
+): Promise<UserDoc> {
   const User: UserModel = createUserModel(db);
-  User.find({ githubId: githubId }).exec((err, users) => {
-    if (err || users.length === 0) {
-      userNotFound();
-    } else {
-      userFound(users[0]);
-    }
-  });
-}
-
-/**
- *
- * @param {mongoose} db The connected MnogoDB object
- * @param {UserDoc} newUser UserDoc to find or save
- * @param {Function} callback Callback passed in from Passport
- */
-export function saveOrFindNewGithubUser(
-  db: typeof mongoose,
-  newUser: UserDoc,
-  callback: Function
-): void {
-  checkUserGithubId(
-    db,
-    newUser.githubId,
-    function (newUser: UserDoc) {
-      newUser.save().then(() => {
-        callback(null, newUser);
-      });
-    },
-    function (foundUser: UserDoc) {
-      callback(null, foundUser);
-    }
-  );
+  const userDoc = await User.findOne({ githubId: newUser.githubId }).exec();
+  if (userDoc) {
+    return userDoc;
+  } else {
+    await newUser.save();
+    return newUser;
+  }
 }
