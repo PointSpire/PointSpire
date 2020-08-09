a2b0abf673f3e91ad28f8747dae37f087b749e63
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-08-09 11:25:58 -0700

    I70 Add date string logic for save file

--- client/src/main/components/TopMenuBar/ImportExportDialog.tsx
+++ client/src/main/components/TopMenuBar/ImportExportDialog.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Debug from 'debug';
 import {
   Dialog,
   DialogTitle,
@@ -6,6 +7,10 @@ import {
   DialogActions,
   Button,
 } from '@material-ui/core';
+import FileSaver from 'file-saver';
+
+const debug = Debug('ImportExportDialog.tsx');
+debug.enabled = true;
 
 export type ImportExportDialogProps = {
   open: boolean;
@@ -19,6 +24,43 @@ function ImportExportDialog(props: ImportExportDialogProps) {
     setOpen(false);
   }
 
+  function get2DigitString(num: number): string {
+    if (num < 10) {
+      return `0${num}`;
+    }
+    return `${num}`;
+  }
+
+  function createDateString(): string {
+    const date = new Date();
+    return (
+      `${date.getFullYear()}` +
+      `${get2DigitString(date.getMonth() + 1)}` +
+      `${get2DigitString(date.getDate())}` +
+      `-${get2DigitString(date.getHours())}` +
+      `${get2DigitString(date.getMinutes())}`
+    );
+  }
+
+  function saveJsonFile() {
+    const blob = new Blob(
+      [
+        JSON.stringify(
+          {
+            someTestValue: 3,
+            someOtherValue: 4,
+          },
+          null,
+          2
+        ),
+      ],
+      {
+        type: 'application/jsoon;charset=utf-8',
+      }
+    );
+    FileSaver.saveAs(blob, `${createDateString()} - pointspire-backup.json`);
+  }
+
   return (
     <Dialog
       open={open}
@@ -27,7 +69,9 @@ function ImportExportDialog(props: ImportExportDialogProps) {
       aria-labelledby="import-export-dialog-title"
     >
       <DialogTitle id="import-export-dialog-title">Import / Export</DialogTitle>
-      <DialogContent dividers />
+      <DialogContent dividers>
+        <Button onClick={saveJsonFile}>Save Backup of Data</Button>
+      </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
           Close
